0952df5a4541013dcd8eeea96e1a11e8e9c8bd50
Abdullah Qureshi <55753562+AbdullahQureshi1080@users.noreply.github.com>
2020-05-19 22:08:06 +0500

    BasicScreenNavigation

--- App.js
+++ App.js
@@ -1,19 +1,73 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { color } from "react-native-reanimated";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+const Stack = createStackNavigator();
+
+class App extends React.Component {
+  
+  state = {};
+  HomeScreen = ({navigation}) => {
+    return (
+      <View style={styles.container}>
+        <Text>Home Screen</Text>
+        <Button title="Next Screen" onPress={()=>{navigation.navigate("Start",{name:"Messi",id:"barca"})}}></Button>
+      </View>
+    );
+  };
+
+  StartScreen = ({navigation, route}) => {
+    const routeAccess = route.params;
+    const id = routeAccess.id;
+
+    return (
+      <View style={styles.container}>
+        <Text>Start Screen</Text>
+        <Text>{id}</Text>
+        <Button title="New ID" onPress={()=>{navigation.setParams({id:"Real"})}}></Button>
+        <Button title="Prev Screen" onPress={()=>{navigation.goBack()}}></Button>
+      </View>
+    );
+  };
+
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator 
+        initialRouteName={"Home"}
+        screenOptions={{
+          headerTitleAlign:"center",
+        }}>
+          <Stack.Screen 
+          name="Home"
+          component={this.HomeScreen}
+          options={{
+            headerTitleAlign:"left",
+            headerTintColor:"white",
+            headerStyle:{
+              backgroundColor:"crimson",
+            },
+            headerRight:()=> <Button title = "Edit"/>
+          }} 
+          />
+          <Stack.Screen name="Start" component={this.StartScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default App;
